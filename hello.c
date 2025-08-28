93cbf3e995c63b81aa829c0909e6146cfd6e41d1
tuckersup <tsup2@huskers.unl.edu>
2025-08-28 17:13:13 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Tucker Sup
+ * Date: 2025/08/28
+ *
+ * Prints my name
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+    printf("Tucker Sup\n");
+
+    return 0;
+}
