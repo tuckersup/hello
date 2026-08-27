bc7d17f0240bedcad26d5d942da8dfe01d8b2e26
tuckersup <tsup2@huskers.unl.edu>
2026-08-27 12:58:57 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Tucker Sup
+ * Date: 2026/08/27
+ *
+ * Hello World in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+    printf("Tucker Sup\n");
+
+    return 0;
+}
